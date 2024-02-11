a5855985d81f7ce0c75c42d66110a7da8cd1b3ac
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-02-11 13:05:58 +0000

    (API)feat: speechRecognitionAPI, add transcription and fixaonsivesive modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,27 +5,32 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
+    id: string
     date: Date
     content: String
   }
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({note}: NoteCardProps) {
+
+export function NoteCard({note, onNoteDeleted}: NoteCardProps) {
   return (
 
 // utilização de componentes da biblioteca radix para criação de um modal
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left bg-slate-800 p-5 flex flex-col gap overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
-        <span className="text-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+        <span className="text-sm font-medium text-slate-300">
+         {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+        </span>
         <p className="text-sm leading-6 text-slate-400">
-        {note.content}
+         {note.content}
         </p>
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none "></div>
       </Dialog.Trigger>
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
-          <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Content className="fixed overflow-hidden inset-0 md-inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
 
             <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400">
               <X className="size-5 hover:text-slate-100"/>
@@ -42,6 +47,7 @@ export function NoteCard({note}: NoteCardProps) {
 
             <button 
             type="button" 
+            onClick={() => onNoteDeleted(note.id)}
 // o atributo group do tailwind aplica estilo aos elementos filhos, como o atributo hover:underline dentro deste botão
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
             >
